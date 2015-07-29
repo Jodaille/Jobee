6117ff59db4edb92707b91d1e0773e95de1a27c6
j_noury <j_noury@MAC0004.local>
2015-07-29 18:22:29 +0200

    add first files

---
+++ src/Beetools/Controller/IndexController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Beetools\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+use Beetools\Model\Beehive;
+
+class IndexController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        $view   = new ViewModel();
+        $oRuche = new Beehive();
+
+        $aCalendars = array();
+        foreach($oRuche->getTypes() as $type)
+        {
+          $aCalendars["$type"] = $oRuche->getCalendarOfType($type);
+          //echo $oRuche->toHtml($aCalendars["$type"]);
+        }
+        $view->aCalendars = $aCalendars;
+        $view->oRuche     = $oRuche;
+
+        return $view;
+    }
+
+}
