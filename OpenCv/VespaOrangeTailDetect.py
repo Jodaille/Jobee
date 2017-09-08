fd6c8e436d25f935b297b00fc369ddae43a658b3
Jodaille <jody.noury@gmail.com>
2017-09-08 17:44:16 +0200

    display images when more found

--- OpenCv/VespaOrangeTailDetect.py
+++ OpenCv/VespaOrangeTailDetect.py
@@ -13,6 +13,8 @@ import sys, os
 # fiile/directory path manipulation
 from os.path import basename, dirname
 
+writeimages = False
+
 # reading image: sys.argv[1] is parameter passed to script
 img = cv2.imread(sys.argv[1], 1)
 
@@ -20,9 +22,11 @@ imgname = basename(sys.argv[1])
 imgdirname = dirname(sys.argv[1])
 base=os.path.basename(sys.argv[1])
 filename = os.path.splitext(base)[0]
-print filename
-maskImgName = imgdirname + '/' + filename + '_mask.jpg'
-blobImgName = imgdirname + '/' + filename + '_blob.jpg'
+resultDir = "/Users/j_noury/Jobee/results"
+print "file:", filename, "imgdirname:", imgdirname
+
+maskImgName = resultDir + '/' + filename + '_mask.jpg'
+blobImgName = resultDir + '/' + filename + '_blob.jpg'
 
 # color conversion
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -32,19 +36,19 @@ upper_range = np.array([34, 255, 255], dtype=np.uint8)
 
 mask = cv2.inRange(hsv, lower_range, upper_range)
 
-print maskImgName
-print blobImgName
-cv2.imwrite(maskImgName, mask)
+print "maskImgName:", maskImgName
+print "blobImgName:", blobImgName
+
 
 # Setup SimpleBlobDetector parameters.
 params = cv2.SimpleBlobDetector_Params()
 # Change thresholds
-params.minThreshold = 10
+params.minThreshold = 15
 params.maxThreshold = 200
 
 # Filter by Area.
 params.filterByArea = True
-params.minArea = 5
+params.minArea = 25
 params.maxArea = 1500
 
 # Filter by Circularity
@@ -54,7 +58,7 @@ params.maxCircularity = 0.22
 
 # Filter by Convexity
 params.filterByConvexity = False
-params.minConvexity = 0.6
+params.minConvexity = 0.8
 
 # Filter by Inertia
 params.filterByInertia = True
@@ -78,13 +82,20 @@ print keypoints
 im_with_keypoints = cv2.drawKeypoints(img, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 
-cv2.imwrite(blobImgName, im_with_keypoints)
+nbpoints = len(keypoints)
+print "Number if VV FOUND:", nbpoints
 
-cv2.imshow('mask',mask)
-cv2.imshow('image', img)
-cv2.imshow('Keypoints', im_with_keypoints)
+if nbpoints == 1 :
+    cv2.imwrite(blobImgName, im_with_keypoints)
+    cv2.imwrite(maskImgName, mask)
+elif nbpoints > 1 :
+    cv2.imshow('mask',mask)
+    cv2.imshow('image', img)
+    cv2.imshow('Keypoints', im_with_keypoints)
+    cv2.waitKey(0)
+else :
+    print "No VV FOUND in", imgname
 
-cv2.waitKey(0)
 #
 # while(1):
 #   k = cv2.waitKey(0)
